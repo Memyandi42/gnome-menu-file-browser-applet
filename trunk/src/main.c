7047b8cc6fd2a84876d1902fe58dd7ce87333782
bertoldia@gmail.com <bertoldia@gmail.com@56a3a253-782c-0410-9e77-497eb458eac0>
2008-04-15 18:58:08 +0000

    Updated info for new release version 0.5.6.

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -34,7 +34,7 @@
 
 #define APPLET_IID			"OAFIID:GNOME_FileBrowserApplet"
 #define APPLET_FACTORY_IID	"OAFIID:GNOME_FileBrowserApplet_Factory"
-#define VERSION				"0.5.5"
+#define VERSION				"0.5.6"
 
 /******************************************************************************/
 static const gchar file_browser_applet_menu_xml [] =

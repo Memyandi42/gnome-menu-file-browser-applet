90e66d520bc516513fb5fb6d88b92d7ad7d0cbd4
bertoldia@gmail.com <bertoldia@gmail.com@56a3a253-782c-0410-9e77-497eb458eac0>
2008-08-14 16:40:17 +0000

    Completed migration to gio. Gnome-vfs dependency is totally gone.Yeya!

--- trunk/src/vfs.h
+++ trunk/src/vfs.h
@@ -23,25 +23,28 @@
  * Boston, MA 02110-1301, USA.
  */
 
-#ifndef VFS_H
-#define VFS_H
+#ifndef __VFS_H__
+#define __VFS_H__
 
 #include <glib.h>
 #include <gtk/gtk.h>
 
 #include "config.h"
 
+/******************************************************************************/
 gboolean vfs_file_is_executable		(const gchar *file_name);
 gboolean vfs_file_is_desktop		(const gchar *file_name);
 gboolean vfs_file_exists			(const gchar *file_name);
 gboolean vfs_file_is_directory		(const gchar *file_name);
 gchar*	 vfs_get_mime_application	(const gchar *file_name_and_path);
-gchar*	 vfs_get_dir_contents		(GPtrArray *files, GPtrArray *dirs, gboolean show_hidden, gchar *path);
+gchar*	 vfs_get_dir_listings		(GPtrArray *files, GPtrArray *dirs, gboolean show_hidden, gchar *path);
 gboolean vfs_launch_desktop_file	(const gchar *file_name);
 void	 vfs_launch_app				(gchar **args, const gchar *working_dir);
 void	 vfs_edit_file				(const gchar *file_name_and_path, gchar *editor_bin);
 void	 vfs_launch_terminal		(const gchar *path, gchar *terminal_bin);
 void	 vfs_open_file				(const gchar *file_name_and_path, gint exec_action);
 void	 vfs_trash_file				(gchar *file_name);
+gchar*	 vfs_get_mime_icon_for_file (const gchar *file_name);
+/******************************************************************************/
 
 #endif

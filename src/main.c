9a1ea6373fd9dd9b6dab83150d8d0b2a9380e6ce
bertoldia <bertoldia@56a3a253-782c-0410-9e77-497eb458eac0>
2008-10-29 17:28:10 +0000

    Updated release notes for upcoming 0.6.0 release.

--- src/main.c
+++ src/main.c
@@ -35,7 +35,7 @@
 
 #define APPLET_IID			"OAFIID:GNOME_FileBrowserApplet"
 #define APPLET_FACTORY_IID	"OAFIID:GNOME_FileBrowserApplet_Factory"
-#define VERSION				"0.5.9"
+#define VERSION				"0.6.0"
 
 /******************************************************************************/
 static const gchar file_browser_applet_menu_xml [] =
